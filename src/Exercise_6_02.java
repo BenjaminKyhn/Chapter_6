3143a9236dc29e1cfe19d309aa75ba73660ba815
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-10-11 10:37:24 +0200

    Exercise 6.02 added

---
+++ src/Exercise_6_02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise_6_02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        long number = input.nextLong();
+        System.out.println(sumDigits(number));
+    }
+
+    public static int sumDigits(long n){
+        int sum = 0;
+        while (n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
