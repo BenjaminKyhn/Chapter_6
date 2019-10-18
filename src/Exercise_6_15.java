436a34b47fcb7e43aed68721d90fd4742ea9f7c7
Benjamin Kyhn <benkyhn@hotmail.com>
2019-10-18 13:58:55 +0200

    Exercise 6.15 added

---
+++ src/Exercise_6_15.java
@@ -0,0 +1,52 @@
+public class Exercise_6_15 {
+    public static void main(String[] args) {
+        System.out.printf("%-20s%-20s%-20s\n", "Taxable Income", "Single", "Married Joint or Qualifying Widow(er)");
+        for (double taxableIncome = 50000; taxableIncome <= 70000; taxableIncome += 100) {
+            System.out.printf("%-20d%-20d%-20d\n", Math.round(taxableIncome), Math.round(computeTax(0, taxableIncome)), Math.round(computeTax(1, taxableIncome)));
+        }
+    }
+
+    private static double computeTax(int status, double taxableIncome) {
+        double tax = 0;
+        if (status == 0) { // Compute tax for single filers
+            if (taxableIncome <= 8350)
+                tax = taxableIncome * 0.10;
+            else if (taxableIncome <= 33950)
+                tax = 8350 * 0.10 + (taxableIncome - 8350) * 0.15;
+            else if (taxableIncome <= 82250)
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (taxableIncome - 33950) * 0.25;
+            else if (taxableIncome <= 171550)
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (taxableIncome - 82250) * 0.28;
+            else if (taxableIncome <= 372950)
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 +
+                        (taxableIncome - 171550) * 0.33;
+            else
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 +
+                        (372950 - 171550) * 0.33 + (taxableIncome - 372950) * 0.35;
+        }
+        else if (status == 1) {
+            if (taxableIncome <= 16700)
+                tax = taxableIncome * 0.10;
+            else if (taxableIncome <= 67900)
+                tax = 16700 * 0.10 + (taxableIncome - 16700) * 0.15;
+            else if (taxableIncome <= 137050)
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (taxableIncome - 67900) * 0.25;
+            else if (taxableIncome <= 208850)
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (taxableIncome - 137050) * 0.28;
+            else if (taxableIncome <= 372950)
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (208850 - 137050) * 0.28 +
+                        (taxableIncome - 208850) * 0.33;
+            else
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (208850 - 137050) * 0.28 +
+                        (taxableIncome - 208850) * 0.33 + (taxableIncome - 372950) * 0.35;
+        }
+        else if (status == 2) { // Too lazy to finish these
+        }
+        else if (status == 3) { // Too lazy to finish these
+        }
+        else {
+            System.out.println("Error: invalid status");
+        }
+        return tax;
+    }
+}
