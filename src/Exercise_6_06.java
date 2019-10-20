5d8c83184ec7b6cae8c52c1ec422c9e6bcb29c7b
Benjamin Kyhn <benkyhn@hotmail.com>
2019-10-20 13:20:48 +0200

    Exercise 6.06 added

--- src/Exercise_6_06.java
+++ src/Exercise_6_06.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Exercise_6_06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int numberOfRows = input.nextInt();
+
+        //Display pattern
+        displayPattern(numberOfRows);
+    }
+
+    //Method for the pattern
     public static void displayPattern(int n){
+        int padding = n - 1; //Holds number of white space
+        for (int i = 1; i <= n; i++) {
+            //Print padding
+            for (int j = 0; j < padding; j++) {
+                System.out.print("  ");
+            }
+
+            //Print numbers
+            for (int k = i; k > 0; k--) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+            padding--;
+
+        }
 
     }
 }
